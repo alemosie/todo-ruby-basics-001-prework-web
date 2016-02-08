96729d096d5804f2db8b67c68dd4ad8c9d9967cf
alemosie <alexandra.siega@gmail.com>
2016-02-07 19:02:45 -0500

    completed test challenges

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+    num1/num2 #auto returns quotient
 end
 
 def assign_variable(value)
-  
+    name = value #assigns value input to name variable
 end
 
-def argue
-  
+def argue(argument)
+    argument = "I'm right and you are wrong!"
 end
 
-def greeting
-
+def greeting(greeting1, greeting2)
+    puts "#{greeting1} #{greeting2}"
 end
 
 def return_a_value
-
+    "Nice"
 end
 
 def last_evaluated_value
-
+    "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping="cheese")
+    topping
 end
